a06ae994503729de7f860bbd117f9b9fde64ffee
sndtvs <sndtvs@gmail.com>
2016-05-18 09:29:41 -0500

    Add files via upload

---
+++ Output.java
@@ -0,0 +1,11 @@
+public interface Output{
+   public void optionMenu(String[] options);
+   
+   public void openingScreen();
+   
+   public void normalText(String playerName, String enemyName, int damage);
+   
+   public void transitionBreak();
+   
+    
+}
---
+++ OutputStorage.java
@@ -0,0 +1,54 @@
+public class OutputStorage{
+   public static void main(String[] args){
+   
+   //*****OPTIONS*****
+      // int listAmount = options.length();
+//       int maxLength = options[0].length();
+//       for(int i = 0; i < options.length(); i++){
+//          if(options[i].length() > maxLength){
+//             maxLength = options[i];
+//          }
+//       }
+//       maxLength = maxLength + 2;
+//       System.out.print(" __| |");//Start top of border
+//       for(int i = 0; i < maxLength; i++){
+//          System.out.print("_");
+//       }
+//       System.out.println("| |__");//End top of border
+//       System.out.print("(__   ");
+//       for(int i = 0; i < maxLength; i++){
+//          System.out.print("_");
+//       }
+//       System.out.println("   __)");
+//       
+//       for(int j = 0; j < listAmount; j++){
+//          System.out.print("   | |");//Start of body
+//          for(int i = 0; i < maxLength; i++){
+//             System.out.print(" ");
+//          }
+//       
+//          System.out.println("| |");
+//       
+//          System.out.println("   | | "+options[j]+" | |"); //Main Text
+//       
+//          System.out.print(" __| |");//
+//          for(int i = 0; i < maxLength; i++){
+//             System.out.print("_");
+//          }
+//          System.out.println("| |__");
+//       }
+//       System.out.print("(__   ");//   __)");
+//       for(int i = 0; i < maxLength; i++){
+//          System.out.print("_");
+//       }
+//       System.out.println("   __)");
+//       System.out.print("   | |");//
+//       for(int i = 0; i < maxLength; i++){
+//          System.out.print(" ");
+//       }
+//       System.out.println("| |");
+
+
+//*****BATTLE TEXT*****
+   }
+}
